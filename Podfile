7d620daa6083663edc73ee0154e3ed2785ce9ae8
jaugeni <jaugeni@gmail.com>
2017-01-17 23:27:16 -0500

    GeoFire installed

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target 'PokeFinder' do
   use_frameworks!
   pod 'Firebase'
   pod 'Firebase/Core'
+  pod 'GeoFire', :git => 'https://github.com/firebase/geofire-objc.git'
   # Pods for PokeFinder
 
 end
--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import GeoFire
 
 class ViewController: UIViewController {
 
+    var geoFire: GeoFire! 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
